849b3da4eb460865eefd2040a4ccbee4104c3114
amirali729 <amirpechs10@gmail.com>
2025-11-25 19:22:59 +0500

    complete the user signup route

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -4,19 +4,51 @@ import { User } from "../models/user.model.js";
 
 export const signupUser = async (req, res) => {
     try {
-        const { username, password } = req.body
+        const { username, password,email } = req.body
 
         if (!username || !password) {
             return res.status(401).json({
                 message: "please provide both username and password"
             })
         }
- 
-        const user = await User.create({username,password})
+
+        if([email,username, password].some((fields) => fields?.trim() === "")){
+            return res.status(401).json({
+                message: "username , password and message can not be empty can not be empty"
+            })
+        }
+
+        const existingUser = await User.findOne({
+            $or : [
+                {username},
+                {email}
+            ]
+        })
+
+        if (existingUser) {
+            if (existingUser.email === email) {
+                return res.status(401).json({
+                    message: "email already exists"
+                })
+            }
+            if (existingUser.username === username) {
+                return res.status(401).json({
+                    message: "username already exists"
+                })
+            }
+        }
+        
+        const user = await User.create({ username, password,email})
 
         const createdUser = await User.findById(user._id)
-        .select("-password")
-        .lean()
+            .select("-password")
+            .lean()
+
+        if (!createdUser) {
+            return res.status(500).json({
+                message:"there some error in creating your account"
+            })
+        }
 
         return res.status(201).json({
             message: "you are account has been created",
--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
+import bcrypt from 'bcrypt'
 
-const  userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
@@ -9,11 +10,41 @@ const  userSchema = new mongoose.Schema({
         lowercase: true,
         trim: true
     },
+    email: {
+        type: String,
+        required: [true, "please provide the email too"],
+        unique: true,
+        lowercase: true,
+        trim: true,
+    },
     password: {
         type: String,
-        required: [true,"password is required please enter your password"]
-
+        required: [true, "password is required please enter your password"]
+    },
+    fullName: {
+        type: String,
+        trim: true
+    },
+    avatar: {
+        type: String,
+    },
+    CoverImage: {
+        type: String,
+    },
+    refreshToken: {
+        type: String
     }
-},{timestamps:true})
 
-export const User = new mongoose.model("User",userSchema)
+
+}, { timestamps: true })
+
+userSchema.pre("save", async function () {
+    if (!this.isModified("password")) return 
+    this.password = await bcrypt.hash(this.password, 10)
+    
+})
+userSchema.methods.isPasswordCorrect = async function (password){
+    return bcrypt.compare(password,this.password)
+}
+
+export const User =  mongoose.model("User", userSchema)
